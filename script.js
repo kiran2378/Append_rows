e44d2929ba99689f08c91d34a6b2b96a20c84ac1
Ravikiran Danappanavar <106485630+kiran2378@users.noreply.github.com>
2022-11-01 16:02:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,12 @@ $(document).ready(function(){
     var country = $("#country").val();
     var row = "<tr><td>" + slno + "</td><td>" + firstname +"</td><td>" + lastname + "</td><td>" + city + "</td><td>" + country + "</td></tr>";
     $("table tbody").append(row);
+         var addSerialNumber = function () {
+      $('table tr').each(function (index) {
+        $(this).find('td:nth-child(1)').html(index - 1 + 1);
+      });
+    };
+    addSerialNumber();
     });
+    
   });
